5a8285780873dec4dd9a1d386238a11284a98513
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2022-04-04 21:31:18 +0300

    added ministats cmp

---
+++ src/components/mini-stats.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="mini-stats">
+    <article v-for="(status, key, idx) in stats" :key="idx">
+      There are {{ status }} leads with status {{ key }}
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'mini-stats',
+  props: {
+    stats: Object,
+  },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <div v-if="leads" class="home">
     <order-overview :leads="leads" />
+    <mini-stats :stats="stats" />
   </div>
 </template>
 
 <script>
 import { leadService } from '@/services/lead-service'
 import OrderOverview from '../components/order-overview.vue'
+import MiniStats from '../components/mini-stats.vue'
 
 export default {
   name: 'Home',
   data() {
     return {
       leads: null,
+      stats: null,
     }
   },
   async created() {
     this.leads = await leadService.query()
+    this.stats = await leadService.getPerStatusStats()
   },
-  components: { OrderOverview },
+  components: { OrderOverview, MiniStats },
 }
 </script>
